f5055d138babe0f1a6db0c33b61d9303827b60e2
edwardsdj <108441141+edwardsdj@users.noreply.github.com>
2022-07-06 23:37:00 -0400

    Update fetch-inventory.sh

--- fetch-inventory.sh
+++ fetch-inventory.sh
@@ -5,7 +5,7 @@ IFS=', ' read -r -a array < dealers.txt
 
 for i in ${array[@]}
 do
-  curl https://www.toyota.com/config/services/inventory/search/getInventory | jq '.[:6]' >> raw.jsonl
+  curl https://www.toyota.com/config/services/inventory/search/getInventory | jq '.[:6]' > raw.jsonl
 done
 
 cat raw.jsonl | jq -s '.' > raw.json
