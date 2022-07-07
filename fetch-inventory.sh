1ea541be332214852a02b7473bffb900ba68d758
edwardsdj <108441141+edwardsdj@users.noreply.github.com>
2022-07-06 23:45:30 -0400

    Update fetch-inventory.sh

--- fetch-inventory.sh
+++ fetch-inventory.sh
@@ -5,6 +5,23 @@ IFS=', ' read -r -a array < dealers.txt
 
 for i in ${array[@]}
 do
+curl \
+  --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+      "brand": "TOY",
+      "mode": "content",
+      "group": true,
+      "groupmode": "full",
+      "relevancy": false,
+      "pagesize": 300,
+      "pagestart": 0,
+      "filter": {
+          "year": [2021, 2022], 
+          "series": ["sienna"], 
+          "dealers": ["'$i'"]
+        }
+    }' \
   curl https://www.toyota.com/config/services/inventory/search/getInventory | jq '.[:6]' >> raw.jsonl
 done
 
